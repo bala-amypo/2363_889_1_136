8ca81db16351c62766a53ae52e57365b858cc1ec
bala-amypo <bala@amypo.in>
2025-12-26 06:29:44 +0000

    User commit

--- src/main/java/com/example/demo/controller/LoanRequestController.java
+++ src/main/java/com/example/demo/controller/LoanRequestController.java
@@ -1,3 +1,12 @@
+import com.example.demo.entity.LoanRequest;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
 @PostMapping
 public LoanRequest submit(@RequestBody LoanRequest request) {
     return service.submitLoanRequest(request);
