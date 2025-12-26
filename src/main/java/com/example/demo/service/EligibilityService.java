aacbcd817e581b19976272122c0814dca0672810
bala-amypo <bala@amypo.in>
2025-12-26 10:04:10 +0000

    User commit

--- src/main/java/com/example/demo/service/EligibilityService.java
+++ src/main/java/com/example/demo/service/EligibilityService.java
@@ -5,4 +5,7 @@ import com.example.demo.entity.EligibilityResult;
 public interface EligibilityService {
 
     EligibilityResult checkEligibility(Long loanRequestId);
+
+    // 🔴 ADD THIS METHOD (required by controller)
+    EligibilityResult getByLoanRequestId(Long loanRequestId);
 }
