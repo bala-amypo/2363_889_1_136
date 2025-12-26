1a098ca6c2c39f659ea4fa4ae0f2511958ed00a2
bala-amypo <bala@amypo.in>
2025-12-26 13:11:52 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -14,7 +14,7 @@ public class OpenApiConfig {
         return new OpenAPI()
                 // You need to change the port as per your server
                 .servers(List.of(
-                        new Server().url("https://9200.408procr.amypo.ai/")
+                        new Server().url("https://9372.pro604cr.amypo.ai/")
                 ));
         }
 }
--- src/main/java/com/example/demo/service/LoanRequestService.java
+++ src/main/java/com/example/demo/service/LoanRequestService.java
@@ -1,14 +1,13 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.LoanRequest;
-
 import java.util.List;
 
 public interface LoanRequestService {
 
-    LoanRequest submitLoanRequest(LoanRequest request);
+    LoanRequest submitRequest(LoanRequest request);
 
     List<LoanRequest> getRequestsByUser(Long userId);
 
-    LoanRequest getRequestById(Long id);
+    LoanRequest getById(Long id);
 }
