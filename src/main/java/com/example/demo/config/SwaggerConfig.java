b6e2eb521eedb3c084e5772252026e32c86fceb3
bala-amypo <bala@amypo.in>
2025-12-27 04:37:57 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,9 +1,11 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -11,9 +13,8 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Loan Management API")
-                        .description("API documentation for Loan Eligibility, EMI and Risk Assessment")
-                        .version("1.0"));
+                .servers(List.of(
+                        new Server().url("https://9372.pro604cr.amypo.ai//")
+                ));
     }
 }
